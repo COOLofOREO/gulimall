b23af0bde232593fc5743b327344a712a6232b21
oreo <hello@world.com>
2024-03-13 20:26:35 +0800

    项目初使文件创建

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -2,12 +2,18 @@ package com.atguigu.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author ~OREO~
+ * @version 1.0
+ * @date 2024/3/1 18:45
+ */
+@EnableDiscoveryClient  //注册服务与发现
 @SpringBootApplication
 public class GulimallCouponApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GulimallCouponApplication.class, args);
+        SpringApplication.run(GulimallCouponApplication.class,args);
     }
-
 }
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,32 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.dao.BrandDao;
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    private BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brand = new BrandEntity();
+        brand.setName("HuaWei");
+        brandService.save(brand);
+        System.out.println("test----");
+    }
+
+    @Test
+    void delete(){
+        BrandEntity byId = brandService.getById(1L);
+        System.out.println("brand:"+byId);
+        brandService.removeById(1L);
+        System.out.println("test--------");
     }
 
 }
